ff55c61b14c3efa14ad05bcbc19de6cf383083a7
SaiJx <xjiaxiang86@gmail.com>
2019-09-30 10:36:14 +0800

    chore(jest): install jest
    1. npm install jest --save-dev
    2. jest --init
    3. support typescript in jest
    4. add other jest library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,32 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    "plugin:xxx-eslint/recommended"
-  ],
+  extends: ['plugin:xxx-eslint/recommended'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
-    project: "./tsconfig.eslint.json"
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
   },
-  plugins: ["@typescript-eslint"],
-  rules: {}
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        ignores: ['modules'],
+      },
+    ],
+    'node/no-missing-import': [
+      'error',
+      {
+        tryExtensions: ['.js', '.json', '.node', '.ts', '.tsx', '.jsx'],
+      },
+    ],
+  },
 };
