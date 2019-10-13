7d1ec1c67842a9318193ea910c2ee5eb3661754b
SaiJx <xjiaxiang86@gmail.com>
2019-10-13 08:35:18 +0800

    feat(@xxx-generate-cli/add): add typescript config

---
+++ packages/add/lib/typescript/index.ts
@@ -0,0 +1,19 @@
+import {
+  extendWorkingDirectoryPackageJson,
+  copyTo,
+} from '@xxx-generate-cli/utils';
+import extendJson from './extend.json';
+import path from 'path';
+
+/**
+ * 安装typescript
+ * @export
+ * @param {string} destination 目标路径
+ * @returns {void}
+ */
+export function installTypeScriptTo(destination: string): void {
+  extendWorkingDirectoryPackageJson(extendJson);
+
+  const tsconfigJsonPath = path.resolve(__dirname, 'tsconfig.json');
+  copyTo([tsconfigJsonPath], destination);
+}
