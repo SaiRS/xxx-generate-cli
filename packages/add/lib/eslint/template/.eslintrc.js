6388169580d5c622c4ee1e7f3596055b83331c2f
SaiJx <xjiaxiang86@gmail.com>
2019-10-19 06:43:53 +0800

    fix(@xxx-generate-cli/add): 更新eslint-plugin-xxx-eslint的版本
    新的xxx-eslint简化了我们的配置工作

--- packages/add/lib/eslint/template/.eslintrc.js
+++ packages/add/lib/eslint/template/.eslintrc.js
@@ -1,55 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
   extends: ['plugin:xxx-eslint/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
-    extraFileExtensions: ['.js', '.ts', 'jsx', 'tsx', '.json'],
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'node/no-unsupported-features/es-syntax': [
-      'error',
-      {
-        ignores: ['modules'],
-      },
-    ],
-    'node/no-missing-import': [
-      'error',
-      {
-        tryExtensions: ['.js', '.json', '.node', '.ts', '.tsx', '.jsx'],
-      },
-    ],
-    'require-jsdoc': [
-      'error',
-      {
-        require: {
-          FunctionDeclaration: true,
-          MethodDefinition: true,
-          ClassDeclaration: true,
-          ArrowFunctionExpression: true,
-          FunctionExpression: true,
-        },
-      },
-    ],
-    'valid-jsdoc': 'error',
-  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      ...require('eslint-plugin-xxx-eslint').configs.typescript,
+    },
+  ],
   settings: {
     polyfills: [
-      // Example of marking entire API and all methods and properties as polyfilled
+      // 解决Object is not iterable的error
       'Promise',
     ],
-    react: {
-      version: 'detect',
-    },
   },
 };
