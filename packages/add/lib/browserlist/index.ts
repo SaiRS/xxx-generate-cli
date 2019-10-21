889c5195d3864708925e1aadf69bafc2fc5e7452
SaiJx <xjiaxiang86@gmail.com>
2019-10-21 11:14:09 +0800

    chore(@xxx-generate-cli/add): browserlist配置从package中移到.browserlistrc

--- packages/add/lib/browserlist/index.ts
+++ packages/add/lib/browserlist/index.ts
@@ -1,11 +1,17 @@
-import pkg from './pkg.json';
-import { extendWorkingDirectoryPackageJson } from '@xxx-generate-cli/utils';
+// import pkg from './pkg.json';
+// import { extendWorkingDirectoryPackageJson } from '@xxx-generate-cli/utils';
+import path from 'path';
+import { copyTo } from '@xxx-generate-cli/utils';
 
 /**
  * 安装browserlist
  * @export
+ * @param {string} destinationPath 目的路径
  * @returns {void}
  */
-export function installBrowserlistTo(): void {
-  extendWorkingDirectoryPackageJson(pkg);
+export function installBrowserlistTo(destinationPath: string): void {
+  // extendWorkingDirectoryPackageJson(pkg);
+
+  const configPath = path.resolve(__dirname, '.browserslistrc');
+  copyTo([configPath], destinationPath);
 }
